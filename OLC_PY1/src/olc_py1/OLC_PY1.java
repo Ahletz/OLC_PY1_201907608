f3d97b2152835e07cdbde09cd8575e01b87b768b
Alexander Ortiz <ludwingaelxander230@gmail.com>
2024-02-13 16:30:54 -0600

    VENTANA INTERFAZ GRAFICA

--- OLC_PY1/src/olc_py1/OLC_PY1.java
+++ OLC_PY1/src/olc_py1/OLC_PY1.java
@@ -1,12 +1,15 @@
 
 package olc_py1;
 
-
 public class OLC_PY1 {
 
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Windows W = new Windows();
+        
+        W.setVisible(true); //VENTANA INTERFAZ
+        
     }
     
 }
---
+++ OLC_PY1/src/olc_py1/Windows.java
@@ -0,0 +1,13 @@
+
+package olc_py1;
+
+import javax.swing.JFrame;
+
+public class Windows extends JFrame{
+    
+    public Windows(){
+        
+        setSize(500,500);
+    }
+    
+}
